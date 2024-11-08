bf69c59a985d67bc7b41ece9727a367c76ef8bb3
Vigitoreanec <chirik39@mail.ru>
2024-11-08 21:38:10 +0200

    fetcher реализован, по user, исписок users, кнопка отображения списка

--- my-next/components/Fetcher/test-fetcher.jsx
+++ my-next/components/Fetcher/test-fetcher.jsx
@@ -5,6 +5,8 @@ import { User } from "../JS-PH/User"
 export function TestFetcher() {
     return <>
         <Test1 />
+        <Test2 />
+        <Test3 />
     </>
 
 
@@ -35,9 +37,25 @@ function Test2() {
         url = 'https://jsonplaceholder.typicode.com/users/',
         [date, setDate] = useState(null);
     return <fieldset>
-        <legend> Fetcher </legend>
+        <legend> Fetcher List </legend>
         <Fetcher url={url} setDate={setDate}>
             <UserList users={date} />
         </Fetcher>
     </fieldset>
+}
+
+function Test3() {
+    const [date, setDate] = useState(null);
+    return <fieldset>
+        <legend>Fetcher Button</legend>
+        {date
+            ? <UserList users={date} />
+            : <button onClick={() => {
+                fetch('https://jsonplaceholder.typicode.com/users/')
+                    .then(res => res.json())
+                    .then(users => setDate(users));
+            }}>load</button>
+        }
+
+    </fieldset >
 }
