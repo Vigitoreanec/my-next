4d360ea6058952d70dbaa1f831fb619e4ba1438a
Vigitoreanec <chirik39@mail.ru>
2024-11-11 12:04:37 +0200

    фильтрация для таблицы Пользователей, включая перевод редактирования телефона

--- my-next/components/ObjectTable/index.jsx
+++ my-next/components/ObjectTable/index.jsx
@@ -1,20 +1,62 @@
+import { useState } from 'react';
 
-export function ObjectTable({ data, columns }) {
-    return <>
-        <table>
-            <thead>
-                <tr>
-                    {columns.map(({ title }) => <td key={title}>{title}</td>)}
-                </tr>
-            </thead>
-            <tbody>
-                {data.map(obj => <tr key={obj.id}>
-                    {columns.map(({ title, content }) =>
-                        <td key={title}>
-                            {content(obj)}
-                        </td>)}
-                </tr>)}
-            </tbody>
-        </table>
-    </>
+export function ObjectTable({ data, columns, children }) {
+  const
+    [search, setSearch] = useState(''),
+    filteredData = data.filter(obj => {
+      if (!search) return true;
+      for (const { content, getData } of columns) {
+        if (content(obj)?.includes?.(search)) return true;
+        if (getData && getData(obj)?.includes?.(search)) return true;
+      }
+      // for (const key in obj) {
+      //   if (obj[key]?.includes?.(search)) return true;
+      // }
+      return false;
+    })
+  return <>
+    <input type="search" value={search} onInput={event => setSearch(event.target.value)} />
+    <SimpleTable data={filteredData} columns={columns}>
+      {children}
+    </SimpleTable>
+  </>
 }
+
+
+
+function SimpleTable({ data, columns, children }) {
+  return <table>
+    <thead>
+      <tr>
+        {columns.map(({ title }) => <td key={title}>{title}</td>)}
+      </tr>
+    </thead>
+    <tbody>
+      {data.map(obj => <tr key={obj.id} data-id={obj.id}>
+        {columns.map(({ title, content }) => <td key={title}>
+          {content(obj)}
+        </td>)}       
+      </tr>)}
+      {children}
+    </tbody>
+  </table>;
+}
+
+// export function ObjectTable({ data, columns }) {
+//     return <table>
+//         <thead>
+//             <tr>
+//                 {columns.map(({ title }) => <td key={title}>{title}</td>)}
+//             </tr>
+//         </thead>
+//         <tbody>
+//             {data.map(obj => <tr key={obj.id} data-id={obj.id}>
+//                 {columns.map(({ title, content }) =>
+//                     <td key={title}>
+//                         {content(obj)}
+//                     </td>)}
+//             </tr>)}
+//         </tbody>
+//     </table>
+
+// }
--- my-next/components/ObjectTable/testTable.jsx
+++ my-next/components/ObjectTable/testTable.jsx
@@ -15,21 +15,67 @@ export function Email({ email }) {
     return <a href={'mailto:' + email}>{email}</a>
 }
 
+export function MapLink({ geo, text }) {
+    return <a href={`https://maps.google.com/maps?ll=${geo.lat},${geo.lng}`} >{text}</a>
+}
+
 function Test1() {
-    const [users, setUsers] = useState(null),
+    const
+        [users, setUsers] = useState(null),
+        [selected, setSelected] = useState(null),
         columns = [
+            { title: '', content: user => String(user.id) == selected ? '✔' : '' },
             { title: 'Id', content: user => +user.id },
+            //{ title: '', content: user => <button onClick={alert(user.company.name + ' | ' + user.company.catchPhrase)}>select</button> },
+
             { title: 'Name', content: ({ name }) => name },
-            { title: 'Phone', content: ({ phone }) => <a href={'tel:phone'}>{phone}</a> },
-            { title: 'Email', content: ({ email }) => <Email email={email} /> }
+            { title: 'Phone', content: ({ phone }) => <a href={'tel:phone'}>{phone}</a>, getData: ({ phone }) => phone },
+            { title: 'Email', content: ({ email }) => <Email email={email} /> },
+            { title: 'Address', content: ({ address }) => <MapLink geo={address.geo} text={`${address.city} ${address.street} ${address.suite}`} /> }
         ];
-
-
-    return <fieldset>
+    return <fieldset onClick={event => {
+        const
+            id = event.target.closest('tbody tr[data-id]')?.dataset?.id;
+        if (id == selected)
+            setSelected(null)
+        else
+            setSelected(id);
+    }}>
         <Fetcher
-            url="https://jsonplaceholder.typicode.com/users/"
-            setDate={setUsers} >
+            url="https://jsonplaceholder.typicode.com/users"
+            setDate={setUsers}
+        >
             <ObjectTable data={users} columns={columns} />
         </Fetcher>
     </fieldset>
 }
+
+
+// function Test1() {
+//     const [users, setUsers] = useState(null),
+//         [selected, setSelected] = useState(null),
+//         columns = [
+//             { title: '', content: user => String(user.id) == selected ? '✔' : '' },
+//             { title: 'Id', content: user => +user.id },
+//             //{ title: '', content: user => <button onClick={alert(user.company.name + ' | ' + user.company.catchPhrase)}>select</button> },
+
+//             { title: 'Name', content: ({ name }) => name },
+//             { title: 'Phone', content: ({ phone }) => <a href={'tel:phone'}>{phone}</a> },
+//             { title: 'Email', content: ({ email }) => <Email email={email} /> },
+//             { title: 'Address', content: ({ address }) => <MapLink geo={address.geo} text={`${address.city} ${address.street} ${address.suite}`} /> }
+//         ];
+
+
+//     return <fieldset onClick={event => {
+//         const
+//             id = event.target.closest('tbody tr[data-id]')?.dataset?.id;
+//         setSelected(id);
+//     }}>
+
+//         <Fetcher
+//             url="https://jsonplaceholder.typicode.com/users/"
+//             setDate={setUsers} >
+//             <ObjectTable data={users} columns={columns} />
+//         </Fetcher>
+//     </fieldset>
+// }
